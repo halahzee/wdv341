f556afed6c39e9a4866cc2e96ad6426bf4f4df70
Halah Azawi <hala.azzawi@gmail.com>
2021-01-28 16:38:46 -0600

    update

--- PHPbasic/phpBasic.php
+++ PHPbasic/phpBasic.php
@@ -6,9 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Create a variable called user name!</h2>
+    <?php echo 'Today Date is:'. " " . date('Y/m/d')?>
+    <h1>PHP Basic</h1>
+    <h3>Create a variable called user name!</h3>
     <?php
-    $yourName = "Halah Alazzawi";
+    $yourName = "<h2>My Name is Halah</h2>";
+    echo $yourName;
+    $number1 = "4";
+    $number2 = "10";
+    echo "Number1" . " = " . $number1;
+    echo "<br><br>";
+    echo "Number2" . " = " . $number2;
+    echo "<br><br>";
+    echo "Number1". "+" . " ". "Number2"." " ."=" . " ";
+    echo "$number1" + "$number2";
     ?>
+    <script>
+        $languages = array["PHP", "HTML" , "JAVASCRIPT"];
+        
+        </script>
 </body>
 </html>
