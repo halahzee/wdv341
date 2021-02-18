f147c83d3b43a1d5fc375a7f4c6d3b50f4a402ed
Halah Azawi <hala.azzawi@gmail.com>
2021-02-18 14:01:49 -0600

    update

---
+++ fileUpload/form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+    <style>
+        form{
+            background: linear-gradient(#e66465, #9198e5);
+            padding: 10%;
+            font-size: 20px;
+        }
+
+        input{
+            padding: 10px;
+            font-size: 20px;
+          
+        }
+    </style>
+</head>
+
+<body>
+       <form action="upload.php" method="post" enctype="multipart/form-data">
+        
+        <h1>WDV341 PHP File Upload</h1><br>
+        <br>
+        <h2>Select file to upload:</h2><br><br>
+        <p>
+        <input type="file" name="fileToUpload" id="fileToUpload"></p>
+        <br><br><br>
+       <p>
+        <input type="submit" value="Upload Image" name="submit">
+       </p> 
+    </form>
+</body>
+</html>
